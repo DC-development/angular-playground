8b9c5b115b614e57da08c7250bcdd44b854017e6
psengelmann <philipp.sengelmann@akquinet.de>
2024-10-24 14:35:45 +0200

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {StartPageComponent} from './components/start-page/start-page.component';
+import {ContactPageComponent} from './components/contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StartPageComponent },
+  { path: 'contact', component: ContactPageComponent }
+];
